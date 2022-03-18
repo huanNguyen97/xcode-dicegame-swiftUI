9c5fdcacc6b73efb3386f84975d3a428d9bd2fa3
Thien.nguyen <thien.nguyen@g-days.net>
2022-03-18 14:05:55 +0700

    OK

--- Dice-Game/ContentView.swift
+++ Dice-Game/ContentView.swift
@@ -8,8 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // Must use state to get Swift knowing this is the data
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 2
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)    // Use to fill all area (over safe area)
+            
+            VStack {
+                Image("diceeLogo")
+                
+                Spacer()
+                
+                HStack {
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                }
+                    .padding(.horizontal)
+                
+                Spacer()
+                
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                    .background(Color.red)
+                
+            }
+        }
+    }
+}
+
+struct DiceView: View {
+    
+    let n: Int
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
             .padding()
     }
 }
@@ -19,3 +66,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
